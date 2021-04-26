2674229165d9735ca08849c5d53cc06cfbe6e234
Vladislav <60598601+Rizzan19@users.noreply.github.com>
2021-04-26 20:42:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+export default (express, bodyParser, createReadStream, crypto, http) => {
+  const app = express();
+  
+  app
+    .use((r, res, next) => r.res.set({
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,POST,PUT,PATCH,OPTIONS,DELETE"}) && next())
+    .use(bodyParser.urlencoded({ extended: true }))
+    .get('/login/', (req, res) => res.send('rizzan18'))   
+    .get('/code/', (req, res) => fs.createReadStream(import.meta.url.substring(7)).pipe(res))
+    .get('/sha1/:input/', r => res.send(crypto.createHash('sha1').update(r.params.input).digest('hex')))
+    .all('/*', r => r.res.send('rizzan18'));
 
+    return app;
+}
