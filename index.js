e3f8e737268a6ca129bafee74ebe32fb7427d395
Vladislav <60598601+Rizzan19@users.noreply.github.com>
2021-05-10 19:21:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-import http from 'http';
 import express from 'express';
 import bodyParser from 'body-parser';
-import { createReadStream } from 'fs';
+import fs from 'fs';
 import crypto from 'crypto';
+import http from 'http';
 
 import appSrc from './app.js';
-
-const app = appSrc(express, bodyParser, createReadStream, crypto, http);
-app.listen(process.env.PORT || 3000);
+const app = appSrc(express, bodyParser, fs, crypto, http);
+app.listen(process.env.PORT);
